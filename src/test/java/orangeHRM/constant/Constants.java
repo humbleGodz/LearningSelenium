46ea789409314e3847ad65254dca30772cf2cb26
humbleGodz <benisales8@gmail.com>
2024-10-14 14:22:14 +0800

    2:21 PM 10/14/2024 Last Update

---
+++ src/test/java/orangeHRM/constant/Constants.java
@@ -0,0 +1,6 @@
+package orangeHRM.constant;
+
+public abstract class Constants {
+	public final static String CURRENT_DIR = System.getProperty("user.dir");
+	public final static String PATH = CURRENT_DIR+ "/src/test/resources/testData/TestData.properties";
+}
